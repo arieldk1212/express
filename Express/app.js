171827a9a6f666d80992183dadc750b90a14200a
Ariel Kriheli <arieldk6@gmail.com>
2023-06-30 18:29:41 +0300

    stuff

--- Express/app.js
+++ Express/app.js
@@ -1,8 +1,14 @@
 // const http = require('http');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 
 // app.use((req, res, next) => {
 //   console.log('1 middleware')
@@ -15,27 +21,16 @@ const app = express();
 // });
  
 
-
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/', (req, res, next) => {
-  console.log('this always runs');
-  next();
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/add-products', (req, res, next) => {
-  console.log('in another middleware');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit>Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) =>{
-
-  res.redirect('/')
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
-app.use('/', (req, res, next) =>{
-  console.log('another another middleware');
-  res.send('<h1>Hi!</h1>');
-});
+
 app.listen(3000);
