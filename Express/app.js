f46484bf1aa3d8dfcc26c9d2b7b693974d0ab6a3
Ariel Kriheli <arieldk6@gmail.com>
2023-07-07 03:06:11 +0300

    added stuff

--- Express/app.js
+++ Express/app.js
@@ -1,27 +1,29 @@
 // const http = require('http');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+// const expressHbs = require("express-handlebars");
 
 const app = express();
 
 // global project variable
-app.set('view engine', 'pug');
-app.set('views', 'views');
 
-const adminData = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+// handle bars is an alternative to pug
+// app.engine("handlebars", expressHbs());
+app.set("view engine", "pug");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop"); 
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);   
 
 app.use((req, res, next) => {
-  res.status(404).render('404');
-})
-
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
 
 app.listen(3000);
